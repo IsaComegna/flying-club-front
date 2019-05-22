4ccec59295b92903f57620c26e522950cfeaae5f
henriquehattori <hattori.noru@gmail.com>
2019-05-22 09:48:54 -0300

    Mudanca de layout

--- src/FlightSubmit.js
+++ src/FlightSubmit.js
@@ -19,22 +19,25 @@ class FlightSubmit extends React.Component {
     // console.log('event',event.target.value);
     this.setState({date: event.target.value});
   }
+
   handleChangeDateTimeStart = (event) => {
     // console.log('event',event.target.value);
     this.setState({dateTimeStart: event.target.value});
   }
+
   handleChangeDateTimeEnd = (event) => {
     // console.log('event',event.target.value);
     this.setState({dateTimeEnd: event.target.value});
   }
+
   handleChangeComment = (event) => {
     // console.log('event',event.target.value);
     this.setState({comment: event.target.value});
   }
 
-
-  handleSubmit = (event) => {
+  handleSubmit =  (event) => {
     event.preventDefault();
+    fetch("https://dog.ceo/api/breeds/image/random");
     // console.log("this.state", this.state);
   }
 
@@ -42,52 +45,64 @@ class FlightSubmit extends React.Component {
     const {matriculaAluno, date, dateTimeEnd, dateTimeStart, comment} = this.state;
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Matricula do Aluno:
-          <input type="text" 
-            value={matriculaAluno}
-            onChange={this.handleChangeMatricula} 
-            />
-        </label>
-        <div>
-          <label>
-            Horário de Inicio :
-          <input type="time" 
-            value={dateTimeStart}
-            onChange={this.handleChangeDateTimeStart}
-            />
+      <div className="page">
+        <span className="title"> Registro de voo </span>
+        <form onSubmit={this.handleSubmit} className="form">
+          <div className="input">
+            <label className="matricula">
+              Matricula do Aluno:
+              <input type="text" 
+                value={matriculaAluno}
+                onChange={this.handleChangeMatricula}
+                className="form-input"
+                />
+            </label>
+          </div>
+          <div className="flight-fields">
+            <div className="input">
               <label>
-                Dia:
-                <input type="date" 
-                  value={date}
-                  onChange={this.handleChangeDate}
-                  />
+                Horário de Inicio:
+              <input type="time" 
+                value={dateTimeStart}
+                onChange={this.handleChangeDateTimeStart}
+                className="form-input"
+                />
+                  <label className="date-field">
+                    Dia:
+                    <input type="date" 
+                      value={date}
+                      onChange={this.handleChangeDate}
+                      className="form-input"
+                      />
+                  </label>
               </label>
-          </label>
-        </div>
-         <div>
-          <label>
-            Horário de Fim:
-          <input type="time" 
-            value={dateTimeEnd}
-            onChange={this.handleChangeDateTimeEnd}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Comenário:
-          <input type="text" 
-            value={this.comment}
-            onChange={this.handleChangeComment}
-            />
-          </label>
-        </div>
-        <div>
-        <input type="submit" value="Registrar"/>
-        </div>
-      </form>
+            </div>
+            <div className="input">
+              <label>
+                Horário de Fim:
+              <input type="time" 
+                value={dateTimeEnd}
+                onChange={this.handleChangeDateTimeEnd}
+                className="form-input"
+                />
+              </label>
+            </div>
+            <div className="input">
+              <label>
+                Comentário:
+              <input type="text" 
+                value={this.comment}
+                onChange={this.handleChangeComment}
+                className="form-input"
+                />
+              </label>
+            </div>
+            <div className="submit-button">
+              <input type="submit" value="Registrar"/>
+            </div>
+          </div>
+        </form>
+      </div>
     );
   }
 }
