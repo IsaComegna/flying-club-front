77e6f7428719caef1d14c3a1abe42c07e7b1a185
henriquehattori <hattori.noru@gmail.com>
2019-06-05 09:12:58 -0300

    adiciona rotas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+
 import logo from './airport.svg';
 import './App.css';
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+//import FlightSubmit from './FlightSubmit';
+import CreateUser from './CreateUser';
+
+
 
 import FlightSubmit from "./FlightSubmit";
 
 function App() {
   return (
-    <div className="App">
-      <FlightSubmit/>
-    </div>
+    <Router>
+      <div>
+        <h2>Boas Vindas ao Flying Club!</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul className="navbar-nav mr-auto">
+          <li><Link to={'/'} className="nav-link"> FlightSubmit </Link></li>
+          <li><Link to={'/registro'} className="nav-link">CreateUser</Link></li>
+        </ul>
+        </nav>
+        <hr />
+        <Switch>
+            <Route exact path='/' component={FlightSubmit} />
+            <Route path='/registro' component={CreateUser} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
